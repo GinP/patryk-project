aca393ac47e7cfeae82fe9500097190f77657f8e
GinP <slawekf6@o2.pl>
2020-03-30 19:43:56 +0200

    userprofile#1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,17 +83,17 @@
                 <div class="title m-b-md">
                     {{ config('app.name') }}
                 </div>
-
+                @guest()
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="{{ route('login') }}">Login</a>
+                    <a href="{{ route('register') }}">Register</a>
                 </div>
+                    @else
+                    <div class="links">
+                        <a href="{{ url('users/'.Auth::user()->id.'/edit')  }}">Your Profile</a>
+                    </div>
+                @endguest
+
             </div>
         </div>
     </body>
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/users/{user}/edit', 'HomeController@edit');
+Route::patch('/users/{user}/edit', 'HomeController@update');
